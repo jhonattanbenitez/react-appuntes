ef27ba23b78912a31ef897d7f51cccddcd146a8f
Jhonattan <jhonattan.benitez@hotmail.com>
2020-07-28 02:15:55 -0500

    appuntes con react + firebase

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyCR0YBzg-YYmUoWzdFQNsGcqz1Tppb-rIU",
+    authDomain: "appuntes-notes.firebaseapp.com",
+    databaseURL: "https://appuntes-notes.firebaseio.com",
+    projectId: "appuntes-notes",
+    storageBucket: "appuntes-notes.appspot.com",
+    messagingSenderId: "949037210000",
+    appId: "1:949037210000:web:f176fadd41d185dc427b15"
+};
